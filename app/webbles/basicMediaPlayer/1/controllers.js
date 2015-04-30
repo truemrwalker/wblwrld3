7c8869a4067098b78472d0360a687572446a317d
Micke Kuwahara <mkuwahara@meme.hokudai.ac.jp>
2015-04-30 16:17:14 +0900

    fixed issue for media file slots when empty

--- app/webbles/basicMediaPlayer/1/controllers.js
+++ app/webbles/basicMediaPlayer/1/controllers.js
@@ -96,7 +96,7 @@ function mediaPlayerWebbleCtrl($scope, $log, Slot, Enum) {
 			'Media Link',
 			'The source link for the media file to be played',
 			$scope.theWblMetadata['templateid'],
-			undefined,
+			{inputType: Enum.aopInputTypes.VideoPick},
 			undefined
 		));
 
