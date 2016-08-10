a7853f3963df5f31dd1a7b57c734f6458c8ea13b
Micke Kuwahara <mkuwahara@meme.hokudai.ac.jp>
2016-08-10 17:09:43 +0900

    minor visual fix in Webble browser and beginning development of Physics engine webble

--- app/webbles/mkuwahara/SimplePhysicsEngine/1/controllers.js
+++ app/webbles/mkuwahara/SimplePhysicsEngine/1/controllers.js
@@ -84,6 +84,8 @@ wblwrld3App.controller('physEngineCtrl', function($scope, $log, Slot, Enum, dbSe
 //    );
 
 
+	var world;
+
 
 
     //=== EVENT HANDLERS ================================================================
@@ -212,6 +214,73 @@ wblwrld3App.controller('physEngineCtrl', function($scope, $log, Slot, Enum, dbSe
 		//TODO: that time may pass and intermediate changes are lost and only the last is sent back to the watch, but sometimes
 		//TODO: that is a good thing when you want the listener to follow the rhythm of Angular and not react too soon.
 		//TODO: So which type to use is up to you and the purpose. Less watches are better for optimization though.
+
+		Physics(function(world){
+
+			var viewWidth = 500;
+			var viewHeight = 300;
+
+			var renderer = Physics.renderer('canvas', {
+				el: 'physEngineViewport',
+				width: viewWidth,
+				height: viewHeight,
+				meta: false, // don't display meta data
+				styles: {
+					// set colors for the circle bodies
+					'circle' : {
+						strokeStyle: '#351024',
+						lineWidth: 1,
+						fillStyle: '#d33682',
+						angleIndicator: '#351024'
+					}
+				}
+			});
+
+			// add the renderer
+			world.add( renderer );
+			// render on each step
+			world.on('step', function(){
+				world.render();
+			});
+
+			// bounds of the window
+			var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
+
+			// constrain objects to these bounds
+			world.add(Physics.behavior('edge-collision-detection', {
+				aabb: viewportBounds,
+				restitution: 0.99,
+				cof: 0.99
+			}));
+
+			// add a circle
+			world.add(
+				Physics.body('circle', {
+					x: 50, // x-coordinate
+					y: 30, // y-coordinate
+					vx: 0.2, // velocity in x-direction
+					vy: 0.01, // velocity in y-direction
+					radius: 20
+				})
+			);
+
+			// ensure objects bounce when edge collision is detected
+			world.add( Physics.behavior('body-impulse-response') );
+
+			// add some gravity
+			world.add( Physics.behavior('constant-acceleration') );
+
+			// subscribe to ticker to advance the simulation
+			Physics.util.ticker.on(function( time, dt ){
+
+				world.step( time );
+			});
+
+			// start the ticker
+			Physics.util.ticker.start();
+
+		});
+
     };
     //===================================================================================
 
@@ -325,6 +394,8 @@ wblwrld3App.controller('physEngineCtrl', function($scope, $log, Slot, Enum, dbSe
 
 
     //=== CTRL MAIN CODE ======================================================================
+
+
 
 });
 //=======================================================================================
